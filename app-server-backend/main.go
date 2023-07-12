3508373c5c7ac8908184badfecabaf0db1c87417
b1018043 <g2122069@fun.ac.jp>
2023-07-13 01:07:57 +0900

    impl add template code

--- app-server-backend/main.go
+++ app-server-backend/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Topic struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.GET("/topics", getTopics)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func getTopics(c echo.Context) error {
+	return c.JSON(http.StatusOK, []Topic{
+		{ID: 1, Title: "Goの使い方"},
+		{ID: 2, Title: "Echoの使い方"},
+	})
 }
