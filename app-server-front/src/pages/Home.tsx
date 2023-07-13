e4f2de473389bb25787fa8170fa6b05d1eeba4a0
b1018043 <g2122069@fun.ac.jp>
2023-07-14 02:14:45 +0900

    chore: add react router

---
+++ app-server-front/src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+type Topic = {
+  id: number
+  title: string
+}
+
+function Home() {
+  const [topics, setTopics] = useState<Topic[]>([])
+
+  useEffect(()=>{
+    // TODO: ここでAPIを叩いてtopicsを更新する
+    setTopics([
+      {
+        id: 1,
+        title: "Reactの使い方"
+      },
+      {
+        id: 2,
+        title: "Viteの使い方"
+      }
+    ])
+  },[])
+
+  return (
+    <>
+      <div>
+        <h1>掲示板アプリ</h1>
+        <h2>話題一覧</h2>
+        <ul>
+          {topics.map((topic) => (
+            <li key={topic.id}>{topic.title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Home
